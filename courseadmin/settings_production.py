9e725fed7b3956c95307e4e4f8645cda6c4244db
danielnjama <danielnjama2015@gmail.com>
2024-04-26 06:06:36 +0300

    docker updates

--- courseadmin/settings_production.py
+++ courseadmin/settings_production.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'courseadmin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'USER': 'djangouser',
+        'PASSWORD': 'userpass',
+        'HOST': 'db',
+        'PORT': '3306',
     },
     'wordpress': {
         'ENGINE': 'django.db.backends.mysql',
